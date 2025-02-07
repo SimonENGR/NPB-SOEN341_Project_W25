773f5e3d1bb219834535cc9768faf8a4c36d9cad
Rohan Kumar <124198883+deadxrk@users.noreply.github.com>
2025-02-07 02:04:24 -0500

    Add files via upload
    This one is for routes, DONOT CONFUSE it with the other present in the source folder.

--- index.js
+++ index.js
@@ -1,40 +1,9 @@
 const express = require('express')
-const cors = require('cors')
+const registerUser = require('../controller/registerUser')
 
-require('dotenv').config()
-const connectDB = require('./config/connectDb')
-const router = require('./routes/index')
+const router = express.Router()
 
-const app = express()
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
+//User Api
+router.post('/register',registerUser)
 
-app.use(express.json())
-
-//Server will run as 3000
-const PORT = process.env.PORT || 3000
-
-//API for testing purpose
-app.get('/',(request,response)=>{
-    response.json({
-        message : "Server running at " + PORT
-    })
-})
-
-//API endpoints
-app.use('/api',router)
-
-
-
-//Also installed one dependency that in case nay changes is made to 
-//current file, Server will run again
-// Dependency : npm i nodemon
-
-//Connectiog Database here
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-        console.log("Our server is runnung at " + PORT)
-    })
-})
+module.exports = router
